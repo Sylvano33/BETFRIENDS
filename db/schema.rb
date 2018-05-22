baecbb99fa4e3d26e97d4ffb18bdf562783aef02
Selim albert <selim.b33@gmail.com>
2018-05-22 12:31:41 +0200

    completed bet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180521153115) do
+ActiveRecord::Schema.define(version: 20180521163045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bets", force: :cascade do |t|
+    t.text "description"
+    t.string "bet_value"
+    t.string "receiver_email"
+    t.integer "status"
+    t.integer "user_id"
+    t.datetime "deadline_acceptation"
+    t.integer "hour_countdown"
+    t.datetime "end_of_bet"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
