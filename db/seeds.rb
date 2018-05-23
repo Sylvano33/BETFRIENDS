66b42652c421f355ec2528ae38ce1697a665aeaf
cb <charley.beigbeder@esc-pau.net>
2018-05-23 11:15:54 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,11 @@ User.destroy_all
 bob = User.create!(username: 'blaireaudu33', email: 'name@example.com', password: 'azerty')
 franck = User.create!(username: 'bgdu33', email: 'name2@example2.com', password: 'azerty')
 
-won = Bet.create!(description: 'bet1', status: 'won', user: bob)
-lost = Bet.create!(description: 'bet2', status: 'lost', user: bob)
+won = Bet.create!(description: 'bet1', bet_value: 'bier', receiver_email: 'name2@example2.com', deadline_acceptation: '02/10/2018', hour_countdown: '24', end_of_bet: '03/10/2018', status: 'won', user: bob)
+lost = Bet.create!(description: 'bet2', bet_value: 'bier', receiver_email: 'name2@example2.com', deadline_acceptation: '02/10/2018', hour_countdown: '24', end_of_bet: '03/10/2018', status: 'lost', user: bob)
 
 UserBet.create!(user: franck, bet: won)
 UserBet.create!(user: franck, bet: lost)
+
+
 
