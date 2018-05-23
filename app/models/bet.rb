550e264b901cd8a501b4c172270c51fc2b31ae07
Sylvain Bretaudeau <sbretaudeau@gmail.com>
2018-05-23 16:09:04 +0200

    new form inputs are all secured :)

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -3,9 +3,14 @@ class Bet < ApplicationRecord
   has_many :user_bets
   enum status: [ :pending, :accepted, :refused, :won, :lost ]
   # scope :won, ->{ where status: :won }
+
+  # EMAIL_REGEX = (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
+
   validates :description, presence: true
   validates :bet_value, presence: true
   validates :receiver_email, presence: true
+  validates_inclusion_of :hour_countdown, :in => 1..48
+  validates_format_of :receiver_email, with:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
   validates :end_date_of_bet, presence: true
 
 end
