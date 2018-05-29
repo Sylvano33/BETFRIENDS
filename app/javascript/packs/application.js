84fe6fc5ecb83c1d9e92854dc47099676274739b
Selim albert <selim.b33@gmail.com>
2018-05-29 02:21:16 +0200

    Brand new date picker, starting day from monday

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
 import "bootstrap";
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css'
+
+const element = document.querySelector('#bet_end_date_of_bet')
+
+if (element) {
+  flatpickr(element, {
+  minDate: "today",
+  dateFormat: "Y-m-d",
+  altInput: true
+
+});
+}
