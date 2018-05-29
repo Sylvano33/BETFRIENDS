f252d2a7d52398b375868625108622dc61ca1206
Selim albert <selim.b33@gmail.com>
2018-05-29 02:38:07 +0200

    DatePicker, french ver

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css'
+import { French } from "flatpickr/dist/l10n/fr.js"
+
 
 const element = document.querySelector('#bet_end_date_of_bet')
 
@@ -8,7 +10,14 @@ if (element) {
   flatpickr(element, {
   minDate: "today",
   dateFormat: "Y-m-d",
-  altInput: true
+  altInput: true,
+  locale: {
+        firstDayOfWeek: 1
+          }
 
 });
 }
+flatpickr(element, {
+    "locale": French // locale for this instance only
+});
+
