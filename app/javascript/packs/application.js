fd0543c4425d05d124d819b18229636d5f7429b6
Selim albert <selim.b33@gmail.com>
2018-05-29 17:13:03 +0200

    first attempt to clipboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,15 +2,20 @@ import "bootstrap";
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css'
 import { French } from "flatpickr/dist/l10n/fr.js"
+import 'dist/clipboard.min.js'
 
 
+
+// DATEPICKR
 const element = document.querySelector('#bet_end_date_of_bet')
 
-flatpickr(element, {
-  "locale": French,
-  minDate: "today",
-  dateFormat: "d-m-y"
-
-});
+if (element) {
+  flatpickr(element, {
+    "locale": French,
+    minDate: "today",
+    dateFormat: "d-m-y",
+    disableMobile: true
+  });
+}
 
 
