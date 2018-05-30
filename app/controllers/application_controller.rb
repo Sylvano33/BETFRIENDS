1a2bdea64aa09805213ff67740f7072ef1cd960b
Sylvain Bretaudeau <sbretaudeau@gmail.com>
2018-05-30 15:43:13 +0200

    title tag settings in Volpeo checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+
+
 private
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
