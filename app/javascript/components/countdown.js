23a3cde0deec1b71f5ae33641b7e79fb2bf5788a
Selim albert <selim.b33@gmail.com>
2018-05-31 18:04:11 +0200

    finalized countdown 3=====O

--- app/javascript/components/countdown.js
+++ app/javascript/components/countdown.js
@@ -1,18 +1,36 @@
-import moment from "moment"
+function getTimeRemaining(endtime) {
+  const t = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((t / 1000) % 60);
+  const minutes = Math.floor((t / 1000 / 60) % 60);
+  const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
 
-function countDown() {
- const counter = document.querySelector("#countdown");
- const start = document.querySelector("#start-time");
-
- if (counter && start) {
-    const time = moment(start.textContent);
-    setInterval(()=>{
-      time.subtract(1, 'seconds');
-      counter.textContent = time.format("HH:mm:ss")
-    }, 1000)
- }
-
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
 }
 
-export { countDown }
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
 
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+
+export { initializeClock };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,11 @@ import { Calendar } from "../components/flatpickr";
 import { HourCountdownOnScroll } from "../components/slider";
 import { initClipboard } from "../components/clipboard";
 import { removeFlashes } from "../components/flashes";
-import { countDown } from "../components/countdown";
+import { initializeClock } from "../components/countdown";
 
 Calendar();
 HourCountdownOnScroll();
 initClipboard();
 removeFlashes();
-countDown();
+const date = document.getElementById('clockdiv').dataset.date;
+initializeClock('clockdiv', date);
